3aa22e19877f23074e22e65bd720224189e0bb11
Hiep Le <hieptuanle@gmail.com>
2023-06-18 22:45:57 +0700

    Add static plugin and some more styles

---
+++ server/plugins/static.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = async function (fastify) {
+  fastify.register(require("@fastify/static"), {
+    root: path.join(__dirname, "../public"),
+    prefix: "/public/",
+  });
+};
